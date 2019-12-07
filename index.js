7b408dce4bf20b854bc015e847d739e74d5ecc87
Abby Personal Acct Howe <howe.abigail@gmail.com>
2019-12-07 15:20:09 -0600

    working timer start, pause and tick

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
 //alert("We are connected!")
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
 
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    this.tick();
+    //this.timer = setInterval(this.tick, 1000);
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
     }
+    // this.timeRemaining = timeRemaining - 1;
+  };
 
-    start = () => {
-        this.interval = setInterval(this.tick, 1000);
-    };
-    tick = () => {
-        this.tick();
-        console.log('Tick');
-    };
-    pause = () => {
-        clearInterval(this.interval);
-    };
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
 
-    importantMethodToCall() {
-        console.log('Important thing completed!');
-    };
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  importantMethodToCall() {
+    console.log("Important thing completed!");
+  }
 }
 
-
-
 const durationInput = document.querySelector("#duration");
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
-timer.start();
+const interval = new Timer(durationInput, startButton, pauseButton);
